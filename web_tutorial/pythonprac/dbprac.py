5c6bb8db0dab8b0892c9b4753efc5bb300708313
kyumlee <kyumlee@student.42seoul.kr>
2023-02-23 12:01:23 +0900

    week5

---
+++ web_tutorial/pythonprac/dbprac.py
@@ -0,0 +1,22 @@
+from pymongo import MongoClient
+import certifi
+
+ca = certifi.where()
+
+# mongoDB에서 connect -> connect your application -> driver: python 3.6 or later -> copy
+client = MongoClient(
+    'mongodb+srv://sparta:test@cluster0.gsiejtz.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
+db = client.dbsparta
+
+doc = {
+    'name': '영수',
+    'age': 24
+}
+db.users.insert_one(doc)
+db.users.insert_one({'name': 'bobby', 'age': 21})
+
+# 데이터마다 id값이 자동으로 생기는데 데이터 추출 시 누락시킬 수 있는 방법
+all_users = list(db.users.find({}, {'_id': False}))
+
+for user in all_users:
+    print(user)
