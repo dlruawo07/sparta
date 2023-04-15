2d5b33e2c098fab4f161f3f81e085a9a2d5afabc
dlruawo07 <dlruawo07@gmail.com>
2023-04-15 13:31:59 +0900

    node.js week1 tutorial (cart API)

--- hh99/chap_3/tutorial/spa_mall/app.js
+++ hh99/chap_3/tutorial/spa_mall/app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const goodsRouter = require("./routes/goods.js");
 
-// req.body를 사용하기 위함 (bodyParser를 쓰기 위한 문법)
-// app.use(express.json());
-app.use("/api", [goodsRouter]);
+const goodsRouter = require("./routes/goods");
+const cartsRouter = require("./routes/carts.js");
+const connect = require("./schemas");
+
+connect();
+
+app.use(express.json());
+
+app.use("/api", [goodsRouter, cartsRouter]);
 
 app.listen(port, () => {
   console.log(port, "포트로 서버가 열렸어요!");
--- hh99/chap_3/tutorial/spa_mall/routes/goods.js
+++ hh99/chap_3/tutorial/spa_mall/routes/goods.js
@@ -1,40 +1,72 @@
 const express = require("express");
 const router = express.Router();
 
-const goods = [
-  {
-    goodsId: 4,
-    name: "상품 4",
-    thumbnailUrl:
-      "https://cdn.pixabay.com/photo/2016/09/07/02/11/frogs-1650657_1280.jpg",
-    category: "drink",
-    price: 0.1,
-  },
-  {
-    goodsId: 3,
-    name: "상품 3",
-    thumbnailUrl:
-      "https://cdn.pixabay.com/photo/2016/09/07/02/12/frogs-1650658_1280.jpg",
-    category: "drink",
-    price: 2.2,
-  },
-  {
-    goodsId: 2,
-    name: "상품 2",
-    thumbnailUrl:
-      "https://cdn.pixabay.com/photo/2014/08/26/19/19/wine-428316_1280.jpg",
-    category: "drink",
-    price: 0.11,
-  },
-  {
-    goodsId: 1,
-    name: "상품 1",
-    thumbnailUrl:
-      "https://cdn.pixabay.com/photo/2016/09/07/19/54/wines-1652455_1280.jpg",
-    category: "drink",
-    price: 6.2,
-  },
-];
+const Cart = require("../schemas/cart.js");
+router.post("/goods/:goodsId/cart", async (req, res) => {
+  const { goodsId } = req.params;
+  const { quantity } = req.body;
+
+  const existsInCart = await Cart.find({ goodsId });
+  if (existsInCart.length) {
+    return res.status(400).json({
+      success: false,
+      message: "카트에 상품이 존재합니다.",
+    });
+  }
+
+  await Cart.create({ goodsId, quantity });
+  res.json({ result: "success" });
+});
+
+router.put("/goods/:goodsId/cart", async (req, res) => {
+  const { goodsId } = req.params;
+  const { quantity } = req.body;
+
+  const existsInCart = await Cart.find({ goodsId });
+  if (existsInCart.length) {
+    await Cart.updateOne(
+      // goodsId에 해당하는 값이 있을 때
+      { goodsId: goodsId },
+      // quantity를 quantity로 바꿔라
+      { $set: { quantity: quantity } }
+    );
+  }
+  res.status(200).json({ success: true });
+});
+
+router.delete("/goods/:goodsId/cart", async (req, res) => {
+  const { goodsId } = req.params;
+
+  const existsInCart = await Cart.find({ goodsId });
+  if (existsInCart.length) {
+    await Cart.deleteOne({ goodsId });
+  }
+  res.status(200).json({ success: true });
+});
+
+const Goods = require("../schemas/goods");
+const cart = require("../schemas/cart");
+router.post("/goods/", async (req, res) => {
+  const { goodsId, name, thumbnailUrl, category, price } = req.body;
+
+  const goods = await Goods.find({ goodsId });
+  console.log(goods);
+  if (goods.length) {
+    return res
+      .status(400)
+      .json({ success: false, errorMessage: "이미 있는 데이터입니다." });
+  }
+
+  const createdGoods = await Goods.create({
+    goodsId,
+    name,
+    thumbnailUrl,
+    category,
+    price,
+  });
+
+  res.json({ goods: createdGoods });
+});
 
 router.get("/goods", (req, res) => {
   res.status(200).json({ goods });
@@ -47,13 +79,5 @@ router.get("/goods/:goodsId", (req, res) => {
   const [result] = goods.filter((good) => good.goodsId === Number(goodsId));
   res.status(200).json({ detail: result });
 });
-
-// router.get("/", (req, res) => {
-//   res.send("default url for goods.js GET Method");
-// });
-
-// router.get("/about", (req, res) => {
-//   res.send("goods.js about PATH");
-// });
 
 module.exports = router;
