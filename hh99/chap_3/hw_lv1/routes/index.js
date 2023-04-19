f62c71f26a1d048201ddfc6fa1da726c520d9d27
dlruawo07 <dlruawo07@gmail.com>
2023-04-19 09:44:14 +0900

    deleted hw_lv1

--- hh99/chap_3/hw_lv1/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", async (req, res) => {
-  return res.send("This is an api index page");
-});
-
-module.exports = router;
